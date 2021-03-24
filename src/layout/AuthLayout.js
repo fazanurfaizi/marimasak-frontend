bd431ebe4d4bd13865f45aa09c8243b3e3c9dee0
Aulia Agustina <auliaguss@gmail.com>
2021-03-24 22:00:46 +0700

    rapi dikit

--- src/layout/AuthLayout.js
+++ src/layout/AuthLayout.js
@@ -9,21 +9,20 @@ const AuthLayout = ({ children }) => {
                     <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 d-none d-sm-none d-lg-block">
                         <div className="land-featurearea">
                             <img src={bowl}/>
-                            <div className="land-meta">
+                            {/* <div className="land-meta">
                                 <h1>Marimasak</h1>
                                 <p>
                                     Marimasak is free to use for as long as you want with two active projects.
                                 </p>
                                 <div className="friend-logo">
-                                    {/* <span><img src={Logo} alt="" /></span> */}
                                 </div>
                                 <a href="#" title="" className="folow-me">Follow Us on</a>
-                            </div>	
+                            </div>	 */}
                             <div className="ikon">
-                                <a>A</a>
-                                <a>A</a>
-                                <a>A</a>
-                                <a>A</a>
+                                <a><i className="fa fa-instagram"></i></a>
+                                <a><i className="fa fa-twitter"></i></a>
+                                <a><i className="fa fa-facebook"></i></a>
+                                <a><i className="fa fa-linkedin"></i></a>
                             </div>
                         </div>
                     </div>
